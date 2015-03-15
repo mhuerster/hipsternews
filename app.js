2c54f24fe79229b4a768969b518ab1df645badd1
mhuerster <mary.huerster@gmail.com>
2015-03-15 11:59:48 -0700

    create posts list

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ angular.module('hipsterNews', [])
   '$scope',
   function($scope){
     $scope.test = 'Hello world!';
+    $scope.posts = [
+    'post1',
+    'post2',
+    'post3',
+    'post4',
+    'post5'
+    ];
   }]);
