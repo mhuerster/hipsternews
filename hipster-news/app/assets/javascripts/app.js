49d02bdce430fda9a9cb11a79ffa5116493e73f5
mhuerster <mary.huerster@gmail.com>
2015-03-16 11:01:36 -0700

    move main controller to separate file

--- hipster-news/app/assets/javascripts/app.js
+++ hipster-news/app/assets/javascripts/app.js
@@ -17,26 +17,6 @@ angular.module('hipsterNews', ['ui.router', 'templates'])
     $urlRouterProvider.otherwise('home');
   }
   ])
-.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function($scope, posts){
-    $scope.test = 'Hello world!';
-    $scope.posts = posts.posts;
-    $scope.addPost = function() {
-      if (!$scope.title || $scope.title == '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes++;
-    };
-  }])
 .controller('PostsCtrl', [
   '$scope',
   '$stateParams',
