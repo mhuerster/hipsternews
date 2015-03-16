c9bcbf4903b5952467201dcf47d5672b83696eee
mhuerster <mary.huerster@gmail.com>
2015-03-16 12:44:42 -0700

    remove placeholder posts

--- hipster-news/app/assets/javascripts/posts/posts.js
+++ hipster-news/app/assets/javascripts/posts/posts.js
@@ -4,48 +4,7 @@ angular.module('hipsterNews')
   function($http){
   // change this variable name to something more semantic
   var o = {
-    posts: [
-      {
-        title: 'post1',
-        upvotes: 5,
-        comments: [
-            {author: 'Joe', body: 'Sriracha YOLO flannel paleo, yr typewriter shabby chic four dollar toast blog PBR dreamcatcher cred master cleanse Thundercats crucifix.', upvotes: 0},
-            {author: 'Bob', body: 'Cardigan gastropub health goth, normcore gentrify you probably haven\'t heard of them DIY Portland typewriter squid twee keytar.', upvotes: 0}
-          ]
-        },
-      {
-        title: 'post2',
-        upvotes: 2,
-        comments: [
-            {author: 'Joe', body: 'Sriracha YOLO flannel paleo, yr typewriter shabby chic four dollar toast blog PBR dreamcatcher cred master cleanse Thundercats crucifix.', upvotes: 0},
-            {author: 'Bob', body: 'Cardigan gastropub health goth, normcore gentrify you probably haven\'t heard of them DIY Portland typewriter squid twee keytar.', upvotes: 0}
-          ]
-        },
-      {
-        title: 'post3',
-        upvotes: 15,
-        comments: [
-            {author: 'Joe', body: 'Sriracha YOLO flannel paleo, yr typewriter shabby chic four dollar toast blog PBR dreamcatcher cred master cleanse Thundercats crucifix.', upvotes: 0},
-            {author: 'Bob', body: 'Cardigan gastropub health goth, normcore gentrify you probably haven\'t heard of them DIY Portland typewriter squid twee keytar.', upvotes: 0}
-          ]
-        },
-      {
-        title: 'post4',
-        upvotes: 9,
-        comments: [
-            {author: 'Joe', body: 'Sriracha YOLO flannel paleo, yr typewriter shabby chic four dollar toast blog PBR dreamcatcher cred master cleanse Thundercats crucifix.', upvotes: 0},
-            {author: 'Bob', body: 'Cardigan gastropub health goth, normcore gentrify you probably haven\'t heard of them DIY Portland typewriter squid twee keytar.', upvotes: 0}
-          ]
-        },
-      {
-        title: 'post5',
-        upvotes: 4,
-        comments: [
-            {author: 'Joe', body: 'Sriracha YOLO flannel paleo, yr typewriter shabby chic four dollar toast blog PBR dreamcatcher cred master cleanse Thundercats crucifix.', upvotes: 0},
-            {author: 'Bob', body: 'Cardigan gastropub health goth, normcore gentrify you probably haven\'t heard of them DIY Portland typewriter squid twee keytar.', upvotes: 0}
-          ]
-        }
-    ]
+    posts: []
   };
   o.getAll = function() {
     return $http.get('/posts.json').success(function(data){
