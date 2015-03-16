96f7b53ea963142aaeb9c16f3440327b66778efa
mhuerster <mary.huerster@gmail.com>
2015-03-16 12:35:41 -0700

    add create and upvote methods add before_filter to load correct post for comment(s)

--- hipster-news/app/controllers/comments_controller.rb
+++ hipster-news/app/controllers/comments_controller.rb
@@ -1,5 +1,22 @@
 class CommentsController < ApplicationController
+  before_filter :load_post
+
+  def create
+    comment = post.comments.create(comment_params)
+    respond_with(post, comment)
+  end
+
+  def upvote
+    comment = post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+    respond_with(post, comment)
+  end
+
 private
+  def load_post
+    post = Post.find(params[:post_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:body)
   end
