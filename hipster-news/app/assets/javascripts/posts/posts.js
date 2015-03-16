8dfa6d8298194b566f7fa2aadcffa0c2315b1d7f
mhuerster <mary.huerster@gmail.com>
2015-03-16 13:18:29 -0700

    add upvote function also fix formatting

--- hipster-news/app/assets/javascripts/posts/posts.js
+++ hipster-news/app/assets/javascripts/posts/posts.js
@@ -1,20 +1,27 @@
 angular.module('hipsterNews')
-.factory('posts', [
-  '$http',
-  function($http){
-  // change this variable name to something more semantic
-  var o = {
-    posts: []
-  };
-  o.getAll = function() {
-    return $http.get('/posts.json').success(function(data){
-      angular.copy(data, o.posts);
-    });
-  };
-  o.create = function(post) {
-    return $http.post('/posts.json', post).success(function(data){
-      o.posts.push(data);
-    });
-  };
-  return o;
-}])
+  .factory('posts', [
+    '$http',
+    function($http) {
+      // change this variable name to something more semantic
+      var o = {
+        posts: []
+      };
+      o.getAll = function() {
+        return $http.get('/posts.json').success(function(data) {
+          angular.copy(data, o.posts);
+        });
+      };
+      o.create = function(post) {
+        return $http.post('/posts.json', post).success(function(data) {
+          o.posts.push(data);
+        });
+      };
+      o.upvote = function(post) {
+        return $http.put('/posts/' + post.id + '/upvote.json')
+          .success(function(data) {
+            post.upvotes += 1;
+          });
+      };
+      return o;
+    }
+  ])
