cc18b4a4d9129c63483bb3dc817a50e2083b24f3
mhuerster <mary.huerster@gmail.com>
2015-03-16 12:32:06 -0700

    add index, show, upvote methods add before_filter to load correct post

--- hipster-news/app/controllers/posts_controller.rb
+++ hipster-news/app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
+  before_filter :load_post, :only => [:show, :upvote]
+
+  def index
+    respond_with(Post.all)
+  end
+
+  def show
+    respond_with(post)
+  end
+
+  def upvote
+    post.increment!(:upvotes)
+    respond_with(post)
+  end
+
+private
+  def load_post
+    post = Post.find(params[:id])
+  end
   def post_params
     params.require(:post).permit(:link, :title)
   end
