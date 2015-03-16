443e54b9f6e7c0bc967057fd99ae8557681d91b9
mhuerster <mary.huerster@gmail.com>
2015-03-15 17:10:45 -0700

    addComment

--- app.js
+++ app.js
@@ -91,4 +91,13 @@ angular.module('hipsterNews', ['ui.router'])
   'posts',
   function($scope, $stateParams, posts){
     $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function() {
+      if ($scope.body == '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
   }]);
