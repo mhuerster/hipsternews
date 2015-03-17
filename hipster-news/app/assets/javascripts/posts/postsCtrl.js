848d89fe817b2ad869122ae3978db838792a4fb0
mhuerster <mary.huerster@gmail.com>
2015-03-17 11:09:15 -0700

    use post from promise in posts state

--- hipster-news/app/assets/javascripts/posts/postsCtrl.js
+++ hipster-news/app/assets/javascripts/posts/postsCtrl.js
@@ -1,17 +1,21 @@
 angular.module('hipsterNews')
-.controller('PostsCtrl', [
-  '$scope',
-  '$stateParams',
-  'posts',
-  function($scope, $stateParams, posts){
-    $scope.post = posts.posts[$stateParams.id];
-    $scope.addComment = function() {
-      if ($scope.body == '') { return; }
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = '';
-    };
-  }]);
+  .controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    'post',
+    function($scope, $stateParams, posts) {
+      $scope.post = post;
+      $scope.addComment = function() {
+        if ($scope.body == '') {
+          return;
+        }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
+    }
+  ]);
