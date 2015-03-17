8cd48f4697fcdeaf47c8053271938bf06d357376
mhuerster <mary.huerster@gmail.com>
2015-03-17 16:05:37 -0700

    login and register functions

--- hipster-news/app/assets/javascripts/auth/authCtrl.js
+++ hipster-news/app/assets/javascripts/auth/authCtrl.js
@@ -4,6 +4,15 @@ angular.module('hipsterNews')
     '$state',
     'Auth',
     function($scope, $state, Auth) {
-
+      $scope.login = function() {
+        Auth.login($scope.user).then(function() {
+          $state.go('home');
+        });
+      };
+      $scope.register = function() {
+        Auth.register($scope.user).then(function() {
+          $state.go('home');
+        });
+      };
     }
   ]);
