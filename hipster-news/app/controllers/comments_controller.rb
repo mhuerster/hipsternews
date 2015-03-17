b22da1e9f812613468c05bdbb68ab35c3121acc8
mhuerster <mary.huerster@gmail.com>
2015-03-17 15:05:21 -0700

    use instance variable

--- hipster-news/app/controllers/comments_controller.rb
+++ hipster-news/app/controllers/comments_controller.rb
@@ -2,19 +2,19 @@ class CommentsController < ApplicationController
   before_filter :load_post
 
   def create
-    comment = post.comments.create(comment_params)
-    respond_with(post, comment)
+    comment = @post.comments.create(comment_params)
+    respond_with(@post, comment)
   end
 
   def upvote
-    comment = post.comments.find(params[:id])
+    comment = @post.comments.find(params[:id])
     comment.increment!(:upvotes)
     respond_with(post, comment)
   end
 
 private
   def load_post
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
   def comment_params
