72a91b04cf9e5c853b55afe2c7acc65dda58fe71
mhuerster <mary.huerster@gmail.com>
2015-03-17 15:27:23 -0700

    install angular-devise, include as dependency

--- hipster-news/app/assets/javascripts/app.js
+++ hipster-news/app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('hipsterNews', ['ui.router', 'templates'])
+angular.module('hipsterNews', ['ui.router', 'templates', 'Devise'])
   .config([
     '$stateProvider',
     '$urlRouterProvider',
