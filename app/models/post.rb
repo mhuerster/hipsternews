adbaaa2ada18a26320457ff41c05e0718831d0ee
mhuerster <mary.huerster@gmail.com>
2015-03-17 23:05:43 -0700

    associate photo with post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  mount_uploader :post_photo, PostPhotoUploader
 
   def as_json(options = {})
     super(options.merge(:include => [:user, :comments => {:include => :user}]))
