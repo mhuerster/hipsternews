9632e9b9d3c556e869433ff8d915376e25f3409c
mhuerster <mary.huerster@gmail.com>
2015-03-17 22:28:01 -0700

    change asset paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 end
