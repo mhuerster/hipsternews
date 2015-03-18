8b7841ddc268713075051f834300fe9a55bdeaba
mhuerster <mary.huerster@gmail.com>
2015-03-17 17:27:30 -0700

    associate post with user

--- hipster-news/db/schema.rb
+++ hipster-news/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317222241) do
+ActiveRecord::Schema.define(version: 20150318002617) do
 
   create_table "comments", force: true do |t|
     t.string   "body"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 20150317222241) do
     t.integer  "upvotes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
