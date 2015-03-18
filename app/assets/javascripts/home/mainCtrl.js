789a2fb6dc149f9f110d286791db07517b10505e
mhuerster <mary.huerster@gmail.com>
2015-03-18 10:00:44 -0700

    INCOMPLETE: add uploader to post create code also delete a line of debugging code

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -2,14 +2,24 @@ angular.module('hipsterNews')
 .controller('MainCtrl', [
   '$scope',
   'posts',
-  function($scope, posts){
-    $scope.test = 'Hello world!';
+  'FileUploader',
+  function($scope, posts, FileUploader){
+    $scope.uploader = new FileUploader('/posts.json');
+    $scope.uploader.onBeforeUploadItem(function(item) {
+      console.log(item);
+    });
     $scope.posts = posts.posts;
     $scope.addPost = function() {
+      if ($scope.uploader.queue[0]) {
+        console.log($scope.uploader.queue[0].file.name);
+        // $scope.uploader.queue[0].upload();
+      }
       if (!$scope.title || $scope.title == '') { return; }
       posts.create({
         title: $scope.title,
         link: $scope.link,
+
+        post_photo: $scope.uploader,
         upvotes: 0
       });
       $scope.title = '';
