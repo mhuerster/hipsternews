28f461753f620626a7f14974055cc44ddc44802f
mhuerster <mary.huerster@gmail.com>
2015-03-17 17:29:11 -0700

    associate comment with user

--- hipster-news/db/schema.rb
+++ hipster-news/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318002617) do
+ActiveRecord::Schema.define(version: 20150318002828) do
 
   create_table "comments", force: true do |t|
     t.string   "body"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 20150318002617) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
