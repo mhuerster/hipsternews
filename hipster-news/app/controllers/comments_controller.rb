f03d5ef033b23b554b838698d1397eabd95b9d1b
mhuerster <mary.huerster@gmail.com>
2015-03-17 17:30:33 -0700

    include user id in post and comment create methods

--- hipster-news/app/controllers/comments_controller.rb
+++ hipster-news/app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :upvote]
 
   def create
-    comment = @post.comments.create(comment_params)
+    comment = @post.comments.create(comment_params.merge(:user_id => current_user.id))
     respond_with(@post, comment)
   end
 
