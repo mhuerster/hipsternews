2e8af08e57cff32590225ab3d70cd5d1a40c3874
mhuerster <mary.huerster@gmail.com>
2015-03-21 20:38:05 -0700

    add photo to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318002828) do
+ActiveRecord::Schema.define(version: 20150318060347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150318002828) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "photo"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
